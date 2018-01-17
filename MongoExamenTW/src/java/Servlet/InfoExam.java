d6dadcb8de580f63c612a754512f8ae2b62be0b6
Monxus <Ramon@LAPTOP-F36EKME9>
2018-01-17 16:56:52 +0100

    Servlet InfoExam created

--- MongoExamenTW/src/java/Servlet/InfoExam.java
+++ MongoExamenTW/src/java/Servlet/InfoExam.java
@@ -5,12 +5,20 @@
  */
 package Servlet;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.bson.Document;
 
 /**
  *
@@ -19,60 +27,68 @@ import javax.servlet.http.HttpServletResponse;
 public class InfoExam extends HttpServlet {
 
     
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+     @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet InfoExam</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet InfoExam at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        MongoClientURI uri = new MongoClientURI(
+                "mongodb+srv://admin:admin@examendb-wge65.mongodb.net/test");
+        MongoClient mongoClient = new MongoClient(uri);
+
+        MongoDatabase database = mongoClient.getDatabase("examen");
+
+        MongoCollection<Document> collection = database.getCollection("examenes");
+//Search document
+        MongoCursor<Document> myDoc = collection.find().iterator();
+
+//Send document
+        response.setContentType("application/json");
+        try (PrintWriter out = response.getWriter()){
+            while (myDoc.hasNext()) {
+                out.println(myDoc.next().toJson());
+            }
+        } finally {
+            myDoc.close();
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+     @Override
+    public void init() {
+        MongoClientURI uri = new MongoClientURI(
+                "mongodb+srv://admin:admin@examendb-wge65.mongodb.net/test");
+        MongoClient mongoClient = new MongoClient(uri);
+
+        MongoDatabase database = mongoClient.getDatabase("examen");
+
+        MongoCollection<Document> collection = database.getCollection("examenes");
+
+        //Crear documentos 
+        Document examenA = new Document("nameExam", "A");
+        Document examenB = new Document("nameExam", "B");
+
+        Document pregunta = new Document("pregunta", "select-1")
+                .append("tipo", "select")
+                .append("titulo", "¿Cuántos centímetros tiene un metro?");
+
+        Document respuesta = new Document("h", 28)
+                .append("0", 10)
+                .append("1", 100)
+                .append("2", 100);
+        pregunta.put("respuesta", respuesta);
+        examenA.put("pregunta", pregunta);
+        examenB.put("pregunta", pregunta);
+        
+        List <Document> docs = new ArrayList<Document>();
+        docs.add(examenA);
+        docs.add(examenB);
+
+        //Insert document
+        collection.insertMany(docs);
+    }
 
 }

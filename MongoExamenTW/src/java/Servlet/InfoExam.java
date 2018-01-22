5e2447d801e593b20e1792df1f7db5718c35ee26
Monxus <Ramon@LAPTOP-F36EKME9>
2018-01-22 17:18:28 +0100

    Updated Servlets

--- MongoExamenTW/src/java/Servlet/InfoExam.java
+++ MongoExamenTW/src/java/Servlet/InfoExam.java
@@ -10,6 +10,10 @@ import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Projections.excludeId;
+import static com.mongodb.client.model.Projections.fields;
+import static com.mongodb.client.model.Projections.include;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -27,49 +31,24 @@ import org.bson.Document;
  */
 public class InfoExam extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        MongoClientURI uri = new MongoClientURI(
-                "mongodb+srv://admin:admin@examendb-wge65.mongodb.net/test");
-        MongoClient mongoClient = new MongoClient(uri);
 
-        MongoDatabase database = mongoClient.getDatabase("examen");
-
-        MongoCollection<Document> collection = database.getCollection("examenes");
-        //Search document
-        MongoCursor<Document> myDoc = collection.find().iterator();
-
-        //Send document
-        response.setContentType("application/json");
-        try (PrintWriter pw = response.getWriter()) {
-            String docAux = "[ ";
-            while (myDoc.hasNext()) {
-                //pw.println(myDoc.next().toJson());
-                docAux += myDoc.next().toJson();
-                docAux += ",";
-            }
-            docAux = docAux.substring(0, docAux.length() - 1);
-            docAux += "]";
-            pw.println(docAux);
-        } finally {
-            myDoc.close();
-            mongoClient.close();
-        }
-    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String dni = request.getParameter("dni");
-        String examName = "A";//request.getParameter(dni);
 
-        //Mas adelante meter comprobación de si ha hecho el examen o no.
-        request.setAttribute("dni", dni);
-        request.setAttribute("examName", examName);
+        String resp = checkExamsAvailables(dni);
 
-        RequestDispatcher a = request.getRequestDispatcher("/index.html");
-        a.forward(request, response);
+        response.setContentType("application/json");
+        try (PrintWriter pw = response.getWriter()) {
+            pw.println(resp);
+        } catch (Exception e) {
+            response.setContentType("application/json");
+            PrintWriter pw = response.getWriter();
+            pw.println("{\"error\":\"Error al cargar los examenes\"}");
+        }
+
     }
 
     @Override
@@ -81,7 +60,10 @@ public class InfoExam extends HttpServlet {
         MongoDatabase database = mongoClient.getDatabase("examen");
 
         MongoCollection<Document> collection = database.getCollection("examenes");
+        MongoCollection<Document> collectionResp = database.getCollection("respuestas");
         collection.drop();
+        collectionResp.drop();
+        
         //Crear documentos 
         Document examenA = new Document("nameExam", "A");
         Document examenB = new Document("nameExam", "B");
@@ -96,14 +78,13 @@ public class InfoExam extends HttpServlet {
                 .append("1", "Por una ventanilla")
                 .append("2", "Por las noticias");
         pregunta1.append("respOpciones", respuesta1);
-        pregunta1.append("respCorrecta", "2");
+        
 
         Document pregunta2 = new Document("id", "p2")
                 .append("tipoPregunta", "text")
                 .append("preguntaTexto", "Te la digo, y no la entiendes; te la vuelvo a repetir y por mucho que te la diga, no la sabes escribir. ¿Qué es?");
 
-        pregunta2.append("respCorrecta", "tela");
-
+        
         Document pregunta3 = new Document("id", "p3")
                 .append("tipoPregunta", "radio")
                 .append("preguntaTexto", "¿Qué es lo primero que hace una vaca cuando sale el Sol?");
@@ -112,9 +93,9 @@ public class InfoExam extends HttpServlet {
                 .append("1", "Sombra")
                 .append("2", "Dormir");
         pregunta3.append("respOpciones", respuesta3);
-        pregunta3.append("respCorrecta", "1");
+        
 
-        //Crear array preguntas
+        //Crear array preguntas !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         List<Document> preguntasA = new ArrayList<Document>();
         preguntasA.add(pregunta1);
 
@@ -124,7 +105,7 @@ public class InfoExam extends HttpServlet {
         List<Document> preguntasC = new ArrayList<Document>();
         preguntasC.add(pregunta3);
         preguntasC.add(pregunta2);
-        preguntasC.add(pregunta1);
+        preguntasC.add(pregunta1);    
 
         //Añadir array preguntas a los examenes
         examenA.append("preguntas", preguntasA);
@@ -132,7 +113,7 @@ public class InfoExam extends HttpServlet {
         examenB.append("preguntas", preguntasB);
 
         examenC.append("preguntas", preguntasC);
-
+        
         List<Document> docs = new ArrayList<Document>();
         docs.add(examenA);
         docs.add(examenB);
@@ -140,8 +121,70 @@ public class InfoExam extends HttpServlet {
 
         //Insert document
         collection.insertMany(docs);
+        
+        //Añadir respuestas
+        pregunta1.append("respCorrecta", "2");
+        pregunta2.append("respCorrecta", "tela");
+        pregunta3.append("respCorrecta", "1");
+        
+        List<Document> docsResp = new ArrayList<Document>();
+        docsResp.add(pregunta1);
+        docsResp.add(pregunta2);
+        docsResp.add(pregunta3);
+
+        //Insert document
+        collectionResp.insertMany(docsResp);
 
         mongoClient.close();
+    }
+
+    private String checkExamsAvailables(String dni) {
+        try {
+            MongoClientURI uri = new MongoClientURI(
+                    "mongodb+srv://admin:admin@examendb-wge65.mongodb.net/test");
+            MongoClient mongoClient = new MongoClient(uri);
+
+            MongoDatabase database = mongoClient.getDatabase("examen");
+
+            MongoCollection<Document> collection = database.getCollection("notas");
+
+            //Search document
+            MongoCursor<Document> myDoc = collection.find(eq("dni", dni)).projection(fields(include("nameExam"), excludeId())).iterator();
+            ArrayList<String> examsName = new ArrayList();
+            examsName.add("A");
+            examsName.add("B");
+            examsName.add("C");
+
+            String docAux = "[ ";
+
+            while (myDoc.hasNext()) {
+                switch (myDoc.next().toJson()) {
+                    case "{ \"nameExam\" : \"A\" }":
+                        examsName.remove(examsName.indexOf("A"));
+                        break;
+                    case "{ \"nameExam\" : \"B\" }":
+                        examsName.remove(examsName.indexOf("B"));
+                        break;
+                    case "{ \"nameExam\" : \"C\" }":
+                        examsName.remove(examsName.indexOf("C"));
+                        break;
+                    default:
+                        break;
+                }
+            }
+            for (int i = 0; i < examsName.size(); i++) {
+                docAux += "{ \"nameExam\" : \"" + examsName.get(i) + "\" },";
+            }
+
+            docAux = docAux.substring(0, docAux.length() - 1);
+            docAux += "]";
+
+            myDoc.close();
+            mongoClient.close();
+            return docAux;
+        } catch (Exception e) {
+            return "{\"error\":\"Error al cargar los examenes\"}";
+        }
     }
 
 }
--- MongoExamenTW/src/java/Servlet/QuestionLoader.java
+++ MongoExamenTW/src/java/Servlet/QuestionLoader.java
@@ -11,8 +11,12 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Projections.excludeId;
+import static com.mongodb.client.model.Projections.fields;
+import static com.mongodb.client.model.Projections.include;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -28,8 +32,8 @@ public class QuestionLoader extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        //String dni = (String) request.getAttribute("dni");
-        String examName = "A";//(String) request.getAttribute("examName");
+        //String dni = (String) request.getAttribute("dni"); ESTO EN TEOIA NO LO NECESITO PARA CAGAR LAS PREGUNTAS DEL EXAMEN
+        String examName = "C";//(String) request.getAttribute("examName"); CAMBIAR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
         MongoClientURI uri = new MongoClientURI(
                 "mongodb+srv://admin:admin@examendb-wge65.mongodb.net/test");
@@ -39,14 +43,16 @@ public class QuestionLoader extends HttpServlet {
 
         MongoCollection<Document> collection = database.getCollection("examenes");
 
-        try (PrintWriter pw = response.getWriter()) { 
+        try (PrintWriter pw = response.getWriter()) {
             //Search document       
-            Document myDoc = collection.find(eq("nameExam", examName)).first();
+            Document myDoc = collection.find(eq("nameExam", examName)).projection(fields(include("preguntas"), excludeId())).first();
+            String docAux = myDoc.toJson();
 
+            docAux = docAux.substring(16, docAux.length() - 1);
             //Send document
             response.setContentType("application/json");
-            
-            pw.println(myDoc.toJson());
+
+            pw.println(docAux);
         } finally {
 
             mongoClient.close();
@@ -57,6 +63,15 @@ public class QuestionLoader extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        //REVISAR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        String examName = request.getParameter("nameExam");
+        String dni = request.getParameter("dni");
+
+        request.setAttribute("dni", dni);
+        request.setAttribute("examName", examName);
+
+        RequestDispatcher a = request.getRequestDispatcher("/index.html");
+        a.forward(request, response);
 
     }
 
